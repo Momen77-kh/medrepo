4ceb634c1bd86e2cc50c0029a4d5f901746da656
Mo'men Khalayleh <momenkhalayleh8@gmail.com>
2025-07-21 04:39:39 +0300

    dynamic function

--- EtlProject.py
+++ EtlProject.py
@@ -1,9 +1,40 @@
 import pandas as pd
-from sqlalchemy import create_engine , text
-from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
 
-engine = create_engine('mysql+pymysql://reporting:Ka8c9*eA:63-GuF%7X@reporting2.cwlsvxzlhirz.eu-west-1.rds.amazonaws.com/medical_inno', echo=True)
+class FetchMedicalInno:
+    def __init__(self):
+        load_dotenv()
+        self.engine = self._create_engine()
 
-df = pd.read_sql("select * from table_name", con=engine)
+    def _create_engine(self):
+        user = os.getenv("MYSQL_USER")
+        password = os.getenv("MYSQL_PASSWORD")
+        host = os.getenv("MYSQL_HOST")
+        port = os.getenv("MYSQL_PORT", 3306)
+        database = os.getenv("MYSQL_DATABASE")
+        return create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}", echo=True)
 
-df.to_csv('results.csv', index=False, encoding='utf-8-sig')
+    def fetch_table(self, table_name, output_file):
+        query = f"SELECT * FROM {table_name} LIMIT 1000"
+        df = pd.read_sql(query, con=self.engine)
+        df.to_csv(output_file, index=False, encoding='utf-8-sig')
+
+    def fetch_all(self):
+        self.fetch_table("consultation", "consultation.csv")
+        self.fetch_table("speciality", "specialty.csv")
+        self.fetch_table("consultation_speciality", "consultation_specialty.csv")
+        self.fetch_table("service_question", "service_question.csv")
+
+    def merge_tables(self):
+        specialty = pd.read_csv("specialty.csv")
+        consultation = pd.read_csv("consultation.csv")
+        consultation_specialty = pd.read_csv("consultation_specialty.csv")
+        service_question = pd.read_csv("service_question.csv")
+
+        merged1 = pd.merge(consultation, consultation_specialty, on='consultation_id', how='inner')
+        merged2 = pd.merge(merged1, specialty, on='specialty_id', how='inner')
+        merged_final = pd.merge(merged2, service_question, on='service_question_id', how='inner')
+
+        merged_final.to_csv('merged_final.csv', index=False, encoding='utf-8-sig')
--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 
+from medicalPrediction import *
+from EtlProject import *
+
+
+def main():
+    medPre = medicalPrediction("momen" , "ألم")
+    medPre.medSpecialty()
+
+
+
+if __name__ == '__main__':
+    main()
